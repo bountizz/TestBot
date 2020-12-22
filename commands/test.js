48ff923b03f284b90e690633ad08d98818e7a7b1
Bountizz <luxoror@gmail.com>
2020-12-22 01:36:50 +0100

    ajout modération dans le embed

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,23 @@
 const Discord = require("discord.js")
+let blackListed = ['enculé', 'filsDePute', 'fdp', 'niqueTaMere', 'niqueTonPere', 'chibrax'];
+let foundInText = false;
 
 module.exports= {
     name: 'test',
     description: "arg message",
     execute(message, args){
-        if (args < 2){
+       
+    for (let i in blackListed){
+        if (message.content.replace(/\s+/g, '').toLowerCase().includes(blackListed[i].replace(/\s+/g, '').toLowerCase())) foundInText = true;
+    }
+
+    if (foundInText) {
+        message.delete();
+        message.channel.send("Alors Non");
+        
+    }else if (args < 2){
             message.channel.send('Veuillez insérer un titre et un sous-titre, vu que jss bloqué la ça va mettre Undefined en sous titre si tu mets que le titre');
-        }else{
+    }else{
         const embed = new Discord.MessageEmbed()
             .addField(args[0], args[1]);
         message.channel.send(embed);
