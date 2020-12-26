bf11f8ffedc52c1d2776e5c93d7d0470314c022b
Bountizz <luxoror@gmail.com>
2020-12-26 21:09:58 +0100

    Fini sans aléatoire

--- commands/rule34.js
+++ commands/rule34.js
@@ -10,11 +10,12 @@ module.exports= {
         
         console.log(xml);
         
-
-        
         let début = xml.indexOf('file_url="') + 'file_url="'.length;
         let fin = xml.lastIndexOf('" parent_id=');
+        if(xml.includes('posts count="0"')){
+            message.channel.send("Aucun post ne correspond à la recherche " +args[0]+" .");
+        }else{
         message.channel.send(xml.substring(début, fin));
-        
+        }
     }
 }
