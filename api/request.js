24f1a74026287f6250468d8ffe3c51798b4e0c64
BoBsmil3Y <antoine-dupont@gmx.fr>
2020-12-30 11:30:33 +0100

    Ajout d'une promise pour gérer la requête. Récupération du json directement dans le fichier de commande.

--- api/request.js
+++ api/request.js
@@ -1,44 +1,35 @@
 const fetch = require('node-fetch');
 const Bluebird = require('bluebird');
 fetch.Promise = Bluebird;
-const xml2js = require('xml2js');
+const parseString = require('xml2js').parseString;
 
 module.exports = {
 
     getImage : async function (tag, random){
         
-        let url = "https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags=" + tag;
+        return new Promise(async (exit, exitWithError) => {
 
-        if(random != null) url = "https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags=" + tag + "&id=" + random;
-        
-        const get_data = async url => {
+            let url = "https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags=" + tag;
+
+            if(random != null) url = "https://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags=" + tag + "&id=" + random;
+                     
+                                
+            const response = await fetch(url);
             
-            try {
-                const response = await fetch(url);
-                
-                if(! response.ok) return new Error("Erreur lors de la récupération de l'image : " + response.statusText);
-                
-                const text = await response.text();
-
-
-
-                const xmlToJson = await xml2js.parseString(text, (err, result) => {
-                    
-                    if(err) throw err;
-
-                    const json = JSON.stringify(result, null, 4);  
-                    
-                    return json;
-                });
-                
-                return xmlToJson;
-
-            } catch (err) {
-                console.error(err);
-            }
-        };
-
-        return get_data(url);
+            if(! response.ok) exitWithError("Erreur lors de la récupération de l'image : " + response.statusText);
             
+            const text = await response.text();
+            
+            
+            let xmlToJson = await parseString(text, (err, result) => {
+                if(err) exitWithError("Erreur durant la conversion en xml : " + err) ;
+
+                const json = JSON.stringify(result, null, 4);  
+
+                exit(json);
+
+            });
+            
+        })
     }
 }
--- commands/rule34.js
+++ commands/rule34.js
@@ -6,38 +6,48 @@ module.exports= {
     description: "Actual great pictures",
     async execute(message, args){
         
-        const xml = await api.getImage(args[0]);
-        
-        //console.log("XML DANS LE RULE34");
-        console.log(xml);
-        
-        /*if(xml.includes('posts count="0"')){
-            message.channel.send('Aucun post ne correspond à la recherche "' +args[0]+'".');
-        }else{*/
-        
-            /*          
-            let débutC = xml.indexOf('posts count="') + 'posts count="'.length;
-            let finC = xml.lastIndexOf('" offset=');
+        api.getImage(args[0]).then(answer => {
             
-            let postCount = xml.substring(débutC, finC);
-            console.log(postCount);
-            let random = Math.ceil(Math.random()*postCount);
-            console.log(random);
-            */
-
-            let début = xml.indexOf('file_url="') + 'file_url="'.length;
-            let fin = xml.lastIndexOf('" parent_id=');
             
+            //Code à mettre ici
 
-            //embed test
-            const embedRule = new Discord.MessageEmbed()
-                .setColor('#bd1111')
-                .setImage(xml.substring(début, fin));
+
+        })
+
+       
+        if(false){
+
+
+            /*if(xml.includes('posts count="0"')){
+                message.channel.send('Aucun post ne correspond à la recherche "' +args[0]+'".');
+            }else{*/
             
-            message.channel.send(embedRule);
+                /*          
+                let débutC = xml.indexOf('posts count="') + 'posts count="'.length;
+                let finC = xml.lastIndexOf('" offset=');
+                
+                let postCount = xml.substring(débutC, finC);
+                console.log(postCount);
+                let random = Math.ceil(Math.random()*postCount);
+                console.log(random);
+                */
 
-            //message.channel.send(xml.substring(début, fin));
+                let début = xml.indexOf('file_url="') + 'file_url="'.length;
+                let fin = xml.lastIndexOf('" parent_id=');
+                
 
-        //}
+                //embed test
+                const embedRule = new Discord.MessageEmbed()
+                    .setColor('#bd1111')
+                    .setImage(xml.substring(début, fin));
+                
+                message.channel.send(embedRule);
+
+                //message.channel.send(xml.substring(début, fin));
+
+            //}
+
+        }
+        
     }
 }
